a654073e20e2c76323f7abda53df2ff4c95552e5
Theron Luhn <theron@luhn.com>
2013-05-11 20:39:44 -0500

    Solve problem 23

--- p023.rb
+++ p023.rb
@@ -1,34 +1,34 @@
+require 'set'
 
-
-class Integer
-  #Monkey patching the builtin Integer class!  Woohoo!
-  #Not exactly necessary, but I wanted to have some fun.
-  def proper_divisor_sum
-    sum = 1 #Already know 1 is a divisor
-    for i in 2..(self/2)
-      if self % i == 0
-        sum += i
-      end
+abundant = Set.new((12..28123).find_all do |n|
+  #Sum the proper divisors
+  sum = 1 #Already know 1 is a divisor
+  for i in 2..(n/2)
+    if n % i == 0
+      sum += i
     end
-    sum
   end
-
-  def abundant?
-    proper_divisor_sum > self
-  end
-end
-
-abundant = (1..28123).find_all{|n| n.abundant?}
-
-puts abundant[0..12]
+  sum > n
+end)
 
 sum = 0
 for n in 1..28123
-  for a in abundant:
-    if a < n
-      continue
+  found = false
+  for a in abundant
+    if found
+      next #There has to be a better way to do this.
     end
+    if a > n
+      next
+    end
+    if abundant.include?(n-a)
+      found = true
+    end
+  end
+  if not found
+    sum += n
   end
 end
 
+puts sum
 
